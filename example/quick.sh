27073c8f83dcb84a9c7b3ca6124d228d9b3ebb93
CheeseLee888 <cheese.lee.888@gmail.com>
2025-12-12 09:47:10 -0600

    Add --chrom: support running step2 by chromosome

--- example/quick.sh
+++ example/quick.sh
@@ -22,26 +22,26 @@ step2_prefix=./${outputFolder}/${phenoCol}_step2
 
 
 ################################# workflow below (do not modify) #################################
-mkdir -p "${outputFolder}"
-# step0: generate GRM from genotype data
-echo "Generating GRM from genotype data..."
-pixi run --manifest-path=../pixi.toml Rscript ../extdata/step0_generateGRM.R \
-    --genoFile=${genoFile} \
-    --grmFile=${grmFile}
+# mkdir -p "${outputFolder}"
+# # step0: generate GRM from genotype data
+# echo "Generating GRM from genotype data..."
+# pixi run --manifest-path=../pixi.toml Rscript ../extdata/step0_generateGRM.R \
+#     --genoFile=${genoFile} \
+#     --grmFile=${grmFile}
 
-# step1: fit null model for all phenotypes
-echo "Fitting null model for all phenotypes..."
-pixi run --manifest-path=../pixi.toml Rscript ../extdata/step1_fitNULL.R \
-    --abdFile=${abdFile} \
-    --covFile=${covFile} \
-    --sampleIDColincovFile=${sampleIDColincovFile} \
-    --grmFile=${grmFile} \
-    --covarColList=${covarColList} \
-    --outputPrefix=${step1_prefix} \
-    --useGRMtoFitNULL=TRUE \
-    --sampleIDColinabdFile=${sampleIDColinabdFile} \
-    --sampleIDColincovFile=${sampleIDColincovFile} \
-    --traitType=count
+# # step1: fit null model for all phenotypes
+# echo "Fitting null model for all phenotypes..."
+# pixi run --manifest-path=../pixi.toml Rscript ../extdata/step1_fitNULL.R \
+#     --abdFile=${abdFile} \
+#     --covFile=${covFile} \
+#     --sampleIDColincovFile=${sampleIDColincovFile} \
+#     --grmFile=${grmFile} \
+#     --covarColList=${covarColList} \
+#     --outputPrefix=${step1_prefix} \
+#     --useGRMtoFitNULL=TRUE \
+#     --sampleIDColinabdFile=${sampleIDColinabdFile} \
+#     --sampleIDColincovFile=${sampleIDColincovFile} \
+#     --traitType=count
 
 
 # step2: score test for phenoCol
@@ -49,6 +49,7 @@ echo "Performing score test for ${phenoCol}..."
 pixi run --manifest-path=../pixi.toml Rscript ../extdata/step2_scoreTest.R \
     --inFile=${genoFile} \
     --phenoCol=${phenoCol} \
+    --chrom=${chrom} \
     --PALMOutputFile=${step2_prefix}.txt \
     --NULLmodelFile=${step1_prefix}.rda \
     --minMAF=0
--- extdata/step2_scoreTest.R
+++ extdata/step2_scoreTest.R
@@ -20,6 +20,14 @@ option_list <- list(
     type = "character", default = "",
     help = "Column name for the phenotype to be tested in the NULLmodelFile."
   ),
+  make_option("--chrom",
+    type = "character", default = "",
+    help = paste(
+      "Chromosome to analyze (e.g. 1, 2, X).",
+      "Currently only supported for PLINK bed/bim/fam input.",
+      "If empty, use all variants in inFile."
+    )
+  ),
   make_option("--NULLmodelFile",
     type = "character", default = "",
     help = "Path to the input file containing the glmm model, which is output from previous step. Will be used by load()"
@@ -42,10 +50,73 @@ opt <- args$options
 print(opt)
 
 
+# --------------------------
+# Handle --chrom / Subset PLINK
+# --------------------------
+inFile_to_use <- opt$inFile  # Default: directly use the user-provided inFile
+
+if (nzchar(opt$chrom)) {
+  message("Chromosome specified: ", opt$chrom)
+
+  # Currently, --chrom is only implemented for PLINK bed/bim/fam
+  bed_file <- paste0(opt$inFile, ".bed")
+  bim_file <- paste0(opt$inFile, ".bim")
+  fam_file <- paste0(opt$inFile, ".fam")
+
+  if (!file.exists(bed_file) || !file.exists(bim_file) || !file.exists(fam_file)) {
+    stop(
+      "--chrom is currently only supported when --inFile is a PLINK prefix. ",
+      "Expected files: ", bed_file, ", ", bim_file, ", ", fam_file
+    )
+  }
+
+  # Temporary prefix: total prefix + .chrX_tmp
+  tmp_prefix <- sprintf("%s.chr%s_tmp", opt$inFile, opt$chrom)
+
+  plink_args <- c(
+    "--bfile", opt$inFile,
+    "--chr",  opt$chrom,
+    "--make-bed",
+    "--out", tmp_prefix
+  )
+
+  message("Subsetting PLINK by chromosome using command:")
+  message("  plink ", paste(plink_args, collapse = " "))
+
+  status <- system2("plink", args = plink_args)
+
+  if (!identical(status, 0L)) {
+    stop("PLINK failed with non-zero exit status: ", status)
+  }
+
+  # Use this sub-prefix to run SPAGMMATtest
+  inFile_to_use <- tmp_prefix
+
+}else{
+  message("No chromosome specified; using all variants in inFile.")
+}
+
+# --------------------------
+# Call SPAGMMATtest
+# --------------------------
 SPAGMMATtest(
-  inFile = opt$inFile,
+  inFile = inFile_to_use,
   phenoCol = opt$phenoCol,
   NULLmodelFile = opt$NULLmodelFile,
   PALMOutputFile = opt$PALMOutputFile,
   minMAF = opt$minMAF
 )
+
+# --------------------------
+# Cleanup temporary files
+# --------------------------
+if (nzchar(opt$chrom)) {
+  tmp_files <- Sys.glob(paste0(tmp_prefix, ".*"))
+  if (length(tmp_files) > 0L) {
+    message(
+      "Cleaning up temporary PLINK files: ",
+      paste(tmp_files, collapse = ", ")
+    )
+    try(file.remove(tmp_files), silent = TRUE)
+  }
+}
