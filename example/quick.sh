e341191d4e9df21f432424ea6392acae179ce889
CheeseLee888 <cheese.lee.888@gmail.com>
2025-12-23 23:28:48 -0500

    output feature_info

--- example/quick.sh
+++ example/quick.sh
@@ -86,3 +86,9 @@ else
         --NULLmodelFile=${step1_prefix}.rda \
         --minMAF=0
 fi
+
+# Step 3: Generate feature information
+pixi run --manifest-path=../pixi.toml Rscript ../extdata/step3_info.R \
+    --abdFile=${abdFile} \
+    --genoFile=${genoFile} \
+    --outputFile=${outputFolder}/feature_info.txt
