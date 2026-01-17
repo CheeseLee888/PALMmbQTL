f6954782dbf47c37cf48127d58d0e3c750f73c9d
CheeseLee888 <cheese.lee.888@gmail.com>
2026-01-17 13:22:45 -0600

    Clean up palm-mbqtl code

--- R/PALMmbQTL_fitNULL.R
+++ R/PALMmbQTL_fitNULL.R
@@ -3,101 +3,28 @@
 #' @param grmFile character. Path to the GRM file in RDS format. The RDS file contains a list with two elements: sample.id and K. sample.id is a vector of sample IDs. K is the GRM matrix with row and column names as sample IDs. By default, "".
 #' @param abdFile character. Path to the abundance file.
 #' @param covFile character. Path to the covariate file.
-#' @param traitType character. e.g. "binary", "quantitative", "count" or "count_nb". By default, "count".
-#' @param invNormalize logical. Whether to perform the inverse normalization for the phentoype or not. e.g. TRUE or FALSE. By default, FALSE
-#' @param covarColList vector of characters. Covariates to be used in the null model. e.g c("Sex", "Age")
-#' @param qCovarCol vector of characters. Categorical covariates to be used in the null model. All categorical covariates listed in qCovarCol must be also in covarColList,  e,g c("Sex").
-#' @param eCovarCol vector of characters. Covariates of environmental factors/cell context to be used in the null model. All covariates listed in eCovarCol must be also in covarColList,  e,g c("cellType").
+#' @param covarColList vector of characters. Covariates to be used in the null model. e.g c("Sex", "Age").
+#' @param offsetCol character. Offset column name in the merged data. If not specified, SeqDepth will be calculated from the abundance file and used as the offset.
 #' @param sampleIDColinabdFile character. Column name for the sample IDs in the abundance file e.g. "IID".
 #' @param sampleIDColincovFile character. Column name for the sample IDs in the covariate file e.g. "IID".
-#' @param cellIDColinphenoFile character. Column name for the cell IDs in the phenotype file e.g. "barcode".
-#' @param tol numeric. The tolerance for fitting the null model to converge. By default, 0.02.
-#' @param maxiter integer. The maximum number of iterations used to fit the null GLMMM. By default, 20.
-#' @param nThreads integer. Number of threads to be used. By default, 1
-#' @param memoryChunk integer or float. The size (Gb) for each memory chunk. By default, 2
-#' @param LOCO logical. Whether to apply the leave-one-chromosome-out (LOCO) option. By default, FALSE
 #' @param outputPrefix character. Path to the output files with prefix.
-#' @param sparseGRMFile character. Path to the pre-calculated sparse GRM file. By default, ""
-#' @param sparseGRMSampleIDFile character. Path to the sample ID file for the pre-calculated sparse GRM. No header is included. The order of sample IDs is corresponding to the order of samples in the sparse GRM. By default, ""
-#' @param isCovariateTransform logical. Whether use qr transformation on non-genetic covariates. By default, TRUE
-#' @param useSparseGRMtoFitNULL logical. Whether to use sparse GRM to fit the null model. By default, FALSE
-#' @param FemaleOnly logical. Whether to run Step 1 for females only. If TRUE, sexCol and FemaleCode need to be specified. By default, FALSE
-#' @param MaleOnly logical. Whether to run Step 1 for males only. If TRUE, sexCol and MaleCode need to be specified. By default, FALSE
-#' @param FemaleCode character. Values in the column for sex (sexCol) in the phenotype file are used for females. By default, '1'
-#' @param MaleCode character. Values in the column for sex (sexCol) in the phenotype file are used for males. By default, '0'
-#' @param sexCol character. Coloumn name for sex in the phenotype file, e.g Sex. By default, ''
 #' @param isCovariateOffset logical. Whether to estimate fixed effect coeffciets. By default, TRUE.
-#' @param isShrinkModelOutput logical. remove unnecessary objects for step2 from the model output. By default, FALSE.
+#' @param useGRMtoFitNULL logical. Whether to use the GRM to fit the NULL model. If FALSE, an identity matrix will be used. By default, TRUE.
 #' @return a file ended with .rda that contains the glmm model information, a file ended with .varianceRatio.txt that contains the variance ratio values, and a file ended with #markers.SPAOut.txt that contains the SPAGMMAT tests results for the markers used for estimating the variance ratio.
 #' @export
 fitNULLGLMM_multiV <- function(grmFile = "",
                                abdFile = "",
                                covFile = "",
-                               isRemoveZerosinPheno = FALSE,
-                               traitType = "count",
-                               invNormalize = FALSE,
                                covarColList = NULL,
-                               qCovarCol = NULL,
-                               eCovarCol = NULL,
                                offsetCol = "",
-                               varWeightsCol = NULL,
-                               longlCol = "",
                                sampleIDColinabdFile = "IID",
                                sampleIDColincovFile = "IID",
-                               cellIDColinphenoFile = "",
-                               tol = 0.02,
-                               maxiter = 20,
-                               nThreads = 1,
-                               memoryChunk = 2,
-                               LOCO = FALSE,
                                outputPrefix = "",
-                               sparseGRMFile = "",
-                               sparseGRMSampleIDFile = "",
-                               isCovariateTransform = FALSE,
-                               useSparseGRMtoFitNULL = FALSE,
-                               sexCol = "",
-                               FemaleCode = 1,
-                               FemaleOnly = FALSE,
-                               MaleCode = 0,
-                               MaleOnly = FALSE,
-                               SampleIDIncludeFile = "",
                                isCovariateOffset = TRUE,
-                               useGRMtoFitNULL = TRUE,
-                               isShrinkModelOutput = FALSE) {
+                               useGRMtoFitNULL = TRUE) {
   ## set up output files
   modelOut <- paste0(outputPrefix, ".rda")
-
   file.create(modelOut, showWarnings = TRUE)
-
-  # if (!useGRMtoFitNULL) {
-  #   useSparseGRMtoFitNULL <- FALSE
-  #   cat("No GRM will be used to fit the NULL model and nThreads is set to 1\n")
-  # }
-
-  if (nThreads > 1) {
-    RcppParallel:::setThreadOptions(numThreads = nThreads)
-    cat(nThreads, " threads will be used ", "\n")
-  }
-  # set_g_omp_num_threads(nThreads)
-
-  if (FemaleOnly & MaleOnly) {
-    stop("Both FemaleOnly and MaleOnly are TRUE. Please specify only one of them as TRUE to run the sex-specific job\n")
-  }
-
-  if (FemaleOnly) {
-    outputPrefix <- paste0(outputPrefix, "_FemaleOnly")
-    cat(
-      "Female-specific model will be fitted. Samples coded as ",
-      FemaleCode, " in the column ", sexCol, " in the phenotype file will be included\n"
-    )
-  } else if (MaleOnly) {
-    outputPrefix <- paste0(outputPrefix, "_MaleOnly")
-    cat(
-      "Male-specific model will be fitted. Samples coded as ",
-      MaleCode, " in the column ", sexCol, " in the phenotype file will be included\n"
-    )
-  }
-
 
   ## sanity checks for required files / arguments -------------------------------
   if (abdFile == "" || !file.exists(abdFile)) {
@@ -128,12 +55,6 @@ fitNULLGLMM_multiV <- function(grmFile = "",
   # - require both tables to contain 'IID'
   # - merge by 'IID' and keep IID as the first column
   merged <- merge_abd_cov(abd, cov)
-
-        # data <- data.table::fread(phenoFile,
-        #   header = T,
-        #   stringsAsFactors = FALSE, colClasses = list(character = sampleIDColinphenoFile), data.table = F, select = checkColList
-        # )
-
   data <- merged
   cat("Abundance and covariate files have been merged\n")
   cat(colnames(data), "\n")
@@ -176,148 +97,14 @@ fitNULLGLMM_multiV <- function(grmFile = "",
       stop("Offset column '", offsetCol, "' contains non-positive values.")
     }
   }
-  
-
-  # if (isRemoveZerosinPheno) {
-  #   data <- data[which(data[, which(colnames(data) == phenoCol)] > 0), ]
-  #   cat("Removing all zeros in the phenotype\n")
-  #   if (nrow(data) == 0) {
-  #     stop("ERROR: no samples are left after removing zeros in the phenotype\n")
-  #   }
-  # }
-
-
-  if (length(qCovarCol) > 0) {
-    cat(qCovarCol, "are categorical covariates\n")
-    if (!all(qCovarCol %in% covarColList)) {
-      stop("ERROR! all covariates in qCovarCol must be in covarColList\n")
-    } else {
-      for (q in qCovarCol) {
-        data[, q] <- as.factor(data[, q])
-      }
-    }
-  }
-
-  if (length(eCovarCol) > 0) {
-    cat(eCovarCol, "are environmental covariates\n")
-    if (!all(eCovarCol %in% covarColList)) {
-      stop("ERROR! all covariates in eCovarCol must be in covarColList\n")
-    }
-  }
 
 
   if (length(covarColList) > 0) {
-    cat(covarColList, "are sample-level covariates\n")
-    if (!all(covarColList %in% covarColList)) {
-      stop("ERROR! all covariates in covarColList must be in covarColList\n")
+    cat("Covariates: ", covarColList, "\n")
+    if (!all(covarColList %in% colnames(data))) {
+      stop("ERROR! all covariates in covarColList must be in the merged data\n")
     }
   }
-
-  if (FemaleOnly | MaleOnly) {
-    if (!sexCol %in% colnames(data)) {
-      stop("ERROR! column for sex ", sexCol, " does not exist in the phenoFile \n")
-    } else {
-      if (FemaleOnly) {
-        data <- data[which(data[, which(colnames(data) ==
-          sexCol)] == FemaleCode), ]
-        if (nrow(data) == 0) {
-          stop(
-            "ERROR! no samples in the phenotype are coded as ",
-            FemaleCode, " in the column ", sexCol,
-            "\n"
-          )
-        }
-      } else if (MaleOnly) {
-        data <- data[which(data[, which(colnames(data) ==
-          sexCol)] == MaleCode), ]
-        if (nrow(data) == 0) {
-          stop(
-            "ERROR! no samples in the phenotype are coded as ",
-            MaleCode, " in the column ", sexCol, "\n"
-          )
-        }
-      }
-    }
-  }
-
-  
-
-  # if (!is.null(sampleListwithGeno)) {
-  #   dataMerge <- merge(mmat_nomissing, sampleListwithGeno,
-  #     by.x = "IID", by.y = "IIDgeno"
-  #   )
-  #   dataMerge_sort <- dataMerge[with(dataMerge, order(IndexGeno)), ]
-  # } else {
-  #   dataMerge_sort <- mmat_nomissing
-  #   dataMerge_sort$IIDgeno <- dataMerge_sort$IID
-  # }
-
-  # if (isCovariateTransform & hasCovariate) {
-  #   cat("qr transformation has been performed on covariates\n")
-  #   out.transform <- Covariate_Transform(formula.null, data = dataMerge_sort)
-  #   formulaNewList <- c(phenoCol, " ~ ", out.transform$Param.transform$X_name[1])
-  #   if (length(out.transform$Param.transform$X_name) > 1) {
-  #     for (i in c(2:length(out.transform$Param.transform$X_name))) {
-  #       formulaNewList <- c(formulaNewList, "+", out.transform$Param.transform$X_name[i])
-  #     }
-  #   }
-  #   formulaNewList <- paste0(formulaNewList, collapse = "")
-  #   formulaNewList <- paste0(formulaNewList, "-1")
-  #   formula.new <- as.formula(paste0(formulaNewList, collapse = ""))
-  #   data.new <- as.data.frame(cbind(out.transform$Y, out.transform$X1))
-  #   colnames(data.new) <- c(phenoCol, out.transform$Param.transform$X_name)
-  #   cat("colnames(data.new) is ", colnames(data.new), "\n")
-  #   cat(
-  #     "out.transform$Param.transform$qrr: ", dim(out.transform$Param.transform$qrr),
-  #     "\n"
-  #   )
-
-  #   if (length(offsetCol) > 0) {
-  #     data.new <- cbind(data.new, dataMerge_sort[, which(colnames(dataMerge_sort) == offsetCol)])
-  #     colnames(data.new)[ncol(data.new)] <- offsetCol
-  #   }
-  # } else {
-  #   formula.new <- formula.null
-  #   data.new <- dataMerge_sort
-  #   out.transform <- NULL
-  # }
-
-
-  # if (useSparseGRMtoFitNULL) {
-  #   if (!isSparseGRMIdentity) {
-  #     getsubGRM_orig(sparseGRMFile, sparseGRMSampleIDFile, relatednessCutoff, dataMerge_sort$IID)
-  #   } else {
-  #     sparseGRM <- Matrix:::sparseMatrix(i = as.vector(1:nrow(data)), j = as.vector(1:nrow(data)), x = rep(1, nrow(data)), symmetric = TRUE)
-  #     rownames(sparseGRM) <- colnames(sparseGRM) <- data[[sampleIDColinphenoFile]]
-  #   }
-  #   gc()
-  # }
-
-
-  # print(dataMerge_sort$IID[1:200])
-  # print(any(duplicated(dataMerge_sort$IID)))
-
-
-  # if (longlCol != "") {
-  #   covarianceIdxMat <- set_covarianceidx_Mat()
-  # } else {
-  #   covarianceIdxMat <- NULL
-  # }
-
-
-  # if (traitType == "binary") {
-  #   stop("ERROR: This traitType is not supported in the current version.\n")
-  # } else if (traitType == "quantitative") {
-  #   stop("ERROR: This traitType is not supported in the current version.\n")
-  # } else if (traitType == "count") {
-  #   cat(phenoCol, " is a count trait\n")
-  #   miny <- min(dataMerge_sort[, which(colnames(dataMerge_sort) == phenoCol)])
-  #   if (miny < 0) {
-  #     stop("ERROR! phenotype value needs to be non-negative \n")
-  #   }
-  # } else if (traitType == "count_nb") {
-  #   stop("ERROR: This traitType is not supported in the current version.\n")
-  # }
 
 
   cat("Start fitting the NULL GLMM\n")
@@ -397,25 +184,6 @@ fitNULLGLMM_multiV <- function(grmFile = "",
       modglmm     = modglmm
     )
   }
-
-  
-  # if (length(eCovarCol) > 0) {
-  #   cat(eCovarCol, "are environmental covariates\n")
-  #   modglmm$eMat <- data.new[, which(colnames(data.new) %in% eCovarCol), drop = F]
-  #   for (em in 1:ncol(modglmm$eMat)) {
-  #     modglmm$eMat[, em] <- (modglmm$eMat[, em] - mean(modglmm$eMat[, em])) / (sd(modglmm$eMat[, em]))
-  #   }
-  # }
-  
-  # if (length(covarColList) > 0) {
-  #   cat(covarColList, "are sample-level covariates\n")
-  
-  #   covarColList <- c(covarColList, sampleCovarCol_q_names)
-  #   modglmm$sampleXMat <- modglmm$X[, which(colnames(modglmm$X) %in% covarColList), drop = F]
-  #   modglmm$sampleXMat <- cbind(modglmm$X[, 1], modglmm$sampleXMat)
-  #   uniqsampleind <- which(!duplicated(modglmm$sampleID))
-  #   modglmm$sampleXMat <- modglmm$sampleXMat[uniqsampleind, ]
-  # }
 
 
   t_end <- proc.time()
--- extdata/step1_fitNULL.R
+++ extdata/step1_fitNULL.R
@@ -24,34 +24,13 @@ option_list <- list(
     type = "character", default = "",
     help = "Required. Path to the covariate file."
   ), 
-  make_option("--isRemoveZerosinPheno",
-    type = "logical", default = FALSE,
-    help = "Optional. Whether to remove zeros in the phenotype"
-  ),
-  make_option("--traitType", type = "character", default = "count", help = "Required. binary or quantitative [default=count]"),
-  make_option("--invNormalize",
-    type = "logical", default = FALSE,
-    help = "Optional. Only for quantitative. Whether to perform the inverse normalization for the phenotype [default='FALSE']"
-  ),
   make_option("--covarColList",
     type = "character", default = "",
     help = "List of covariates (comma separated)"
   ),
-  make_option("--qCovarColList",
-    type = "character", default = "",
-    help = "List of categorical covariates (comma separated). All categorical covariates must also be in covarColList"
-  ),
   make_option("--offsetCol",
     type = "character", default = "",
     help = "offset column"
-  ),
-  make_option("--varWeightsCol",
-    type = "character", default = NULL,
-    help = "variance weight column"
-  ),
-  make_option("--longlCol",
-    type = "character", default = "",
-    help = ""
   ),
   make_option("--sampleIDColinabdFile",
     type = "character", default = "IID",
@@ -61,83 +40,15 @@ option_list <- list(
     type = "character", default = "IID",
     help = "Column name of sample IDs in the covariate file, e.g. IID"
   ),
-  make_option("--cellIDColinphenoFile",
-    type = "character", default = "",
-    help = "Column name of cell IDs in the phenotype file, e.g. barcode"
-  ),
-  make_option("--tol",
-    type = "numeric", default = 0.02,
-    help = "Optional. Tolerance for fitting the null GLMM to converge [default=0.02]."
-  ),
-  make_option("--maxiter",
-    type = "integer", default = 20,
-    help = "Optional. Maximum number of iterations used to fit the null GLMM [default=20]."
-  ),
-  make_option("--nThreads",
-    type = "integer", default = 1,
-    help = "Optional. Number of threads (CPUs) to use [default=1]."
-  ),
-  make_option("--memoryChunk",
-    type = "numeric", default = 2,
-    help = "Optional. Size (Gb) for each memory chunk [default=2]"
-  ),
-  make_option("--LOCO",
-    type = "logical", default = FALSE,
-    help = "Whether to apply the leave-one-chromosome-out (LOCO) approach when fitting the null model using the full GRM [default=FALSE]."
-  ),
   make_option("--outputPrefix",
     type = "character", default = "~/",
     help = "Required. Path and prefix of the output files [default='~/']"
-  ),
-  make_option("--sparseGRMFile",
-    type = "character", default = "",
-    help = "Path to the pre-calculated sparse GRM file. If not specified and  IsSparseKin=TRUE, sparse GRM will be computed [default=NULL]"
-  ),
-  make_option("--sparseGRMSampleIDFile",
-    type = "character", default = "",
-    help = "Path to the sample ID file for the pre-calculated sparse GRM. No header is included. The order of sample IDs is corresponding to sample IDs in the sparse GRM [default=NULL]"
-  ),
-  make_option("--isCovariateTransform",
-    type = "logical", default = FALSE,
-    help = "Optional. Whether use qr transformation on covariates [default='FALSE']."
-  ),
-  make_option("--useSparseGRMtoFitNULL",
-    type = "logical", default = FALSE,
-    help = "Optional. Whether to use sparse GRM to fit the null model [default='FALSE']."
-  ),
-  make_option("--sexCol",
-    type = "character", default = "",
-    help = "Optional. Column name for sex in the phenotype file, e.g Sex"
-  ),
-  make_option("--FemaleCode",
-    type = "character", default = "1",
-    help = "Optional. Values in the column for sex in the phenotype file are used for females [default, '1']"
-  ),
-  make_option("--FemaleOnly",
-    type = "logical", default = FALSE,
-    help = "Optional. Whether to run Step 1 for females only [default=FALSE]. if TRUE, --sexCol and --FemaleCode need to be specified"
-  ),
-  make_option("--MaleCode",
-    type = "character", default = "0",
-    help = "Optional. Values in the column for sex in the phenotype file are used for males [default, '0']"
-  ),
-  make_option("--MaleOnly",
-    type = "logical", default = FALSE,
-    help = "Optional. Whether to run Step 1 for males only [default=FALSE]. if TRUE, --sexCol and --MaleCode need to be specified"
-  ),
-  make_option("--SampleIDIncludeFile",
-    type = "character", default = "",
-    help = "Path to the file that contains one column for IDs of samples who will be include for null model fitting."
   ),
   make_option("--isCovariateOffset",
     type = "logical", default = TRUE,
     help = "Optional. Whether to estimate fixed effect coeffciets. [default, 'TRUE']"
   ),
-  make_option("--useGRMtoFitNULL", type = "logical", default = TRUE, help = ""),
-  make_option("--isShrinkModelOutput",
-    type = "logical", default = FALSE,
-    help = "Optional. Whether to remove unnecessary objects for step2 from the model output. [default, 'FALSE']"
-  )
+  make_option("--useGRMtoFitNULL", type = "logical", default = TRUE, help = "")
 )
 
 
@@ -167,69 +78,28 @@ if (opt$covarColList!="all") {
   print(covars)
 }
 
-qcovars <- strsplit(opt$qCovarColList, ",")[[1]]
-convertoNumeric <- function(x, stringOutput) {
-  y <- tryCatch(expr = as.numeric(x), warning = function(w) {
-    return(NULL)
-  })
-  if (is.null(y)) {
-    stop(stringOutput, " is not numeric\n")
-  } else {
-    cat(stringOutput, " is ", y, "\n")
-  }
-  return(y)
-}
-
-
-# BLASctl_installed <- require(RhpcBLASctl)
-# if (BLASctl_installed) {
-#   # Set number of threads for BLAS to 1, this step does not benefit from multithreading or multiprocessing
-#   original_num_threads <- blas_get_num_procs()
-#   blas_set_num_threads(1)
+# convertoNumeric <- function(x, stringOutput) {
+#   y <- tryCatch(expr = as.numeric(x), warning = function(w) {
+#     return(NULL)
+#   })
+#   if (is.null(y)) {
+#     stop(stringOutput, " is not numeric\n")
+#   } else {
+#     cat(stringOutput, " is ", y, "\n")
+#   }
+#   return(y)
 # }
 
-
-# set seed
 set.seed(1)
 fitNULLGLMM_multiV(
   grmFile = opt$grmFile,
   abdFile = opt$abdFile,
   covFile = opt$covFile,
-  isRemoveZerosinPheno = opt$isRemoveZerosinPheno,
-  traitType = opt$traitType,
-  invNormalize = opt$invNormalize,
   covarColList = covars,
-  qCovarCol = qcovars,
   offsetCol = opt$offsetCol,
-  varWeightsCol = opt$varWeightsCol,
-  longlCol = opt$longlCol,
   sampleIDColinabdFile = opt$sampleIDColinabdFile,
   sampleIDColincovFile = opt$sampleIDColincovFile,
-  cellIDColinphenoFile = opt$cellIDColinphenoFile,
-  tol = opt$tol,
-  maxiter = opt$maxiter,
-  nThreads = opt$nThreads,
-  memoryChunk = opt$memoryChunk,
-  LOCO = opt$LOCO,
   outputPrefix = opt$outputPrefix,
-  sparseGRMFile = opt$sparseGRMFile,
-  sparseGRMSampleIDFile = opt$sparseGRMSampleIDFile,
-  isCovariateTransform = opt$isCovariateTransform,
-  useSparseGRMtoFitNULL = opt$useSparseGRMtoFitNULL,
-  sexCol = opt$sexCol,
-  FemaleCode = opt$FemaleCode,
-  FemaleOnly = opt$FemaleOnly,
-  MaleCode = opt$MaleCode,
-  MaleOnly = opt$MaleOnly,
-  SampleIDIncludeFile = opt$SampleIDIncludeFile,
   isCovariateOffset = opt$isCovariateOffset,
-  useGRMtoFitNULL = opt$useGRMtoFitNULL,
-  isShrinkModelOutput = opt$isShrinkModelOutput
+  useGRMtoFitNULL = opt$useGRMtoFitNULL
 )
-
-
-
-# if (BLASctl_installed) {
-#   # Restore originally configured BLAS thread count
-#   blas_set_num_threads(original_num_threads)
-# }
