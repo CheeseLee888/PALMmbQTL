427b3d467301d4d615a71b2ebb9092342ccd59f9
CheeseLee888 <cheese.lee.888@gmail.com>
2026-01-23 16:31:53 -0600

    Add all-chrom result from palm2 and re-generate plot/

--- example/step3.sh
+++ example/step3.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 ################################# parameter settings below #################################
-inFile=./output/palm2_step2_chr1_g1.txt
-outdir=./plot
+inFile=output/palm2_step2_g1.txt
+outdir=plot
 
 
 
 ################################# workflow below (do not modify) #################################
+echo "Start: Generate plots."
 pixi run --manifest-path=../pixi.toml Rscript ../extdata/step3_plot.R \
     --inFile=${inFile} \
     --outdir=${outdir}
+echo "Finish: Generate plots."
