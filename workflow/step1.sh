75dc2b482bb5447a3e83b03127c8cd7dd9871db3
CheeseLee888 <cheese.lee.888@gmail.com>
2026-01-27 22:43:03 -0600

    Move config.sh to work dir

--- workflow/step1.sh
+++ workflow/step1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${SCRIPT_DIR}/config.sh"
+source "${SCRIPT_DIR}/../config.sh"
 
 
 ################################# workflow below (do not modify) #################################
