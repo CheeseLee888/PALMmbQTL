7657947a1ec44c7de804dd410c072d00d7567c27
CheeseLee888 <cheese.lee.888@gmail.com>
2026-01-27 19:55:01 -0600

    Change config input to relative and construct them to later

--- workflow/config.sh
+++ workflow/config.sh
@@ -2,40 +2,52 @@
 ################################# parameter settings below #################################
 ##################################### required below #######################################
 # PALM method; 1 for 'PALM' or 2 for 'PALM-mbQTL'
-PALMmethod=2 
+PALMmethod=2
 
-# input and output folders
+# input and output folders (Relative to WORK)
 inputFolder=example/input
 outputFolder=example/output
 
-# input files
-genoFile=${inputFolder}/geno
-abdFile=${inputFolder}/abd.txt
-covFile=${inputFolder}/cov.txt
+# input files (Relative to inputFolder)
+genoFile=geno
+abdFile=abd.txt
+covFile=cov.txt
 
 # chromosome number to analyze (for step2 only); set to 'NULL' for all chromosomes
-chrom=NULL
+chrom=1
 
 ##################################### optional below #######################################
 # Default paths for other files, can be modified as needed
-grmFile=${inputFolder}/grm.rds
-outputSnpFile=${outputFolder}/info_snp.txt
-outputFeatureFile=${outputFolder}/info_feature.txt
-outputSampleFile=${outputFolder}/info_sample.txt
-palm1_step1_prefix=${outputFolder}/palm1_step1_allpheno
-palm2_step1_prefix=${outputFolder}/palm2_step1_allpheno
+grmFile=grm.rds
+outputSnpFile=info_snp.txt
+outputFeatureFile=info_feature.txt
+outputSampleFile=info_sample.txt
+palm1_step1_prefix=palm1_step1_allpheno
+palm2_step1_prefix=palm2_step1_allpheno
 if [[ "${chrom}" == "NULL" ]]; then
-  palm1_step2_prefix=${outputFolder}/palm1_step2_allchr
-  palm2_step2_prefix=${outputFolder}/palm2_step2_allchr
+  palm1_step2_prefix=palm1_step2_allchr
+  palm2_step2_prefix=palm2_step2_allchr
 else
-  palm1_step2_prefix=${outputFolder}/palm1_step2_chr${chrom}
-  palm2_step2_prefix=${outputFolder}/palm2_step2_chr${chrom}
+  palm1_step2_prefix=palm1_step2_chr${chrom}
+  palm2_step2_prefix=palm2_step2_chr${chrom}
 fi
 
 
 
 
 ###################### export variables for scripts (do not modify) ########################
+genoFile=${inputFolder}/${genoFile}
+abdFile=${inputFolder}/${abdFile}
+covFile=${inputFolder}/${covFile}
+grmFile=${inputFolder}/${grmFile}
+outputSnpFile=${outputFolder}/${outputSnpFile}
+outputFeatureFile=${outputFolder}/${outputFeatureFile}
+outputSampleFile=${outputFolder}/${outputSampleFile}
+palm1_step1_prefix=${outputFolder}/${palm1_step1_prefix}
+palm2_step1_prefix=${outputFolder}/${palm2_step1_prefix}
+palm1_step2_prefix=${outputFolder}/${palm1_step2_prefix}
+palm2_step2_prefix=${outputFolder}/${palm2_step2_prefix}
+
 export PALMmethod
 export inputFolder
 export outputFolder
